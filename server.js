cd116a1a46fc6d10c0e699db5397f8f0ba8fea8f
Daniyal-90 <daniyalnaeem.ak@gmail.com>
2026-05-05 23:59:15 +0500

    api/index.js-modified

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
-const { V1_PATH } = require('./shared/constants');
-const mainRoutes = require("./routes/index");
+//const { V1_PATH } = require('./shared/constants');
+//const mainRoutes = require("./routes/index");
 
 const app = express();
 
