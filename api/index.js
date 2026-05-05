f1c6dcc900c713f36278ec2eff3a8aa48f2c8056
Daniyal-90 <daniyalnaeem.ak@gmail.com>
2026-05-05 22:34:40 +0500

    api/index.js-server.js-modified

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
-import app from '../server.js';
+import serverless from "serverless-http";
+import app, { connectDB } from "../server.js";
 
-export default function handler(req, res) {
-  return app(req, res);
+export default async function handler(req, res) {
+  await connectDB();
+  return serverless(app)(req, res);
 }
