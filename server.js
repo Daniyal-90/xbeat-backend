3974a1bd0a1080a06e5647b6129230db1c77eb6a
Daniyal-90 <daniyalnaeem.ak@gmail.com>
2026-05-05 23:06:53 +0500

    server.js-modified

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ export async function connectDB() {
   await mongoose.connect(process.env.MONGODB_URI);
   isConnected = true;
 
-  console.log("MongoDB connected ✅");
+  console.log("MongoDB connectedddd ✅");
 }
 
 /**
